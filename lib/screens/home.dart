4cd45c233cfee77cddb76c8406c565334d2df6ae
kchatr <kchatr1729@gmail.com>
2021-09-18 09:02:48 -0400

    Created Bottom Nav Bar for Pictures

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -10,20 +11,31 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final ImagePicker _picker = ImagePicker();
+  final ImagePicker picker = ImagePicker();
+
+  File? imageFile;
+
+  Future<void> pickImage(ImageSource source) async {
+    XFile? selFile = await picker.pickImage(source: source);
+    File selected = File(selFile!.path);
+
+    setState(() {
+      imageFile = selected;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Calorie Counter"),
-        backgroundColor: Colors.purple[400],
+        backgroundColor: Colors.purple[300],
       ),
       body: Container(
         height: 240.0,
         decoration: const BoxDecoration(
           image: DecorationImage(
-              image: AssetImage("images/home_bg.jpg"), fit: BoxFit.fill),
+              image: AssetImage("assets/images/home_bg.jpg"), fit: BoxFit.fill),
         ),
         child: Center(
           child: Text(
@@ -38,14 +50,37 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          //TODO: Allow users to choose from gallery too
-          _picker.pickImage(source: ImageSource.camera);
-        },
-        tooltip: "Take or Select an Image",
-        backgroundColor: Colors.purple,
-        child: const Icon(Icons.add_a_photo, color: Colors.white),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () {
+      //     //TODO: Allow users to choose from gallery too
+      //     _picker.pickImage(source: ImageSource.camera);
+      //   },
+      //   tooltip: "Take or Select an Image",
+      //   backgroundColor: Colors.purple,
+      //   child: const Icon(Icons.add_a_photo, color: Colors.white),
+      // ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.purple[100],
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            IconButton(
+              onPressed: () {
+                pickImage(ImageSource.camera);
+              },
+              color: Colors.purple[100],
+              icon: const Icon(Icons.add_a_photo, color: Colors.white),
+            ),
+            IconButton(
+              onPressed: () {
+                pickImage(ImageSource.gallery);
+              },
+              color: Colors.purple[100],
+              icon: const Icon(Icons.photo_library, color: Colors.white),
+            ),
+          ],
+        ),
       ),
     );
   }
