a95ea51186b07c8529dba7e2502a1692aae90972
kchatr <kchatr1729@gmail.com>
2021-09-18 12:23:00 -0400

    Created API to Retrieve Info + Sliding Panel for Displaying

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -5,6 +5,8 @@ import 'package:image_picker/image_picker.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
 
+import 'package:sliding_up_panel/sliding_up_panel.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -23,9 +25,6 @@ class _HomePageState extends State<HomePage> {
 
     var response = await http.get(uri);
 
-    // print('Response status: ${response.statusCode}');
-    // print('Response body: ${response.body}');
-
     final body = json.decode(response.body);
 
     return body;
@@ -39,20 +38,8 @@ class _HomePageState extends State<HomePage> {
       imageFile = selected;
     });
 
-    // var url = Uri.parse("https://api.edamam.com/api/nutrition-data");
-    // var url = Uri.parse(
-    //     "https://api.edamam.com/api/nutrition-data?app_id=e45357af&app_key=b93730855e301e7ba5518d4b4c707e9a&nutrition_type=logging&ingr=1%20slice%20chocolate%20cake");
-    // var response = await http.get(url);
-
-    // print('Response status: ${response.statusCode}');
-    // print('Response body: ${response.body}');
-
-    // final body = json.decode(response.body);
-
     nutritionInfo = await getInfo(
         "https://api.edamam.com/api/nutrition-data?app_id=e45357af&app_key=b93730855e301e7ba5518d4b4c707e9a&nutrition_type=logging&ingr=1%20slice%20chocolate%20cake");
-
-    print(nutritionInfo);
   }
 
   @override
@@ -62,40 +49,101 @@ class _HomePageState extends State<HomePage> {
         title: const Text("Calorie Counter"),
         backgroundColor: Colors.purple[300],
       ),
-      body: ListView(
-        children: [
-          Container(
-            height: 240.0,
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage("assets/images/home_bg.jpg"),
-                  fit: BoxFit.fill),
-            ),
-            child: Center(
-              child: Text(
-                "Welcome to CalorMe!",
+      body: SlidingUpPanel(
+        color: Colors.teal.shade300,
+        borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(22.0), topRight: Radius.circular(22.0)),
+        minHeight: 30.0,
+        panel: Column(
+          children: [
+            Text("Nutritional Info",
                 textAlign: TextAlign.center,
-                style: GoogleFonts.questrial(
-                  textStyle: const TextStyle(
-                    fontSize: 40.0,
-                    color: Colors.white,
+                style: GoogleFonts.rubik(
+                  fontSize: 19.0,
+                  color: Colors.white,
+                )),
+            const SizedBox(height: 30),
+            Text(
+              "Calories: ${nutritionInfo?['calories'] ?? 'N/A'}",
+              style: GoogleFonts.quicksand(
+                fontSize: 17.5,
+                fontWeight: FontWeight.w500,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 6),
+            Text(
+              "Fat: ${nutritionInfo?['totalNutrients']['FAT']['quantity'] ?? 'N/A'} g",
+              style: GoogleFonts.quicksand(
+                fontSize: 17.5,
+                fontWeight: FontWeight.w500,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 6),
+            Text(
+              "Fiber: ${nutritionInfo?['totalNutrients']['FIBTG']['quantity'] ?? 'N/A'} g",
+              style: GoogleFonts.quicksand(
+                fontSize: 17.5,
+                fontWeight: FontWeight.w500,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 6),
+            Text(
+              "Protein: ${nutritionInfo?['totalNutrients']['PROCNT']['quantity'] ?? 'N/A'} g",
+              style: GoogleFonts.quicksand(
+                fontSize: 17.5,
+                fontWeight: FontWeight.w500,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 6),
+            Text(
+              "Carbs: ${nutritionInfo?['totalNutrients']['CHOCDF']['quantity'] ?? 'N/A'} g",
+              style: GoogleFonts.quicksand(
+                fontSize: 17.5,
+                fontWeight: FontWeight.w500,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 6),
+            Text(
+              "Cholesterol: ${nutritionInfo?['totalNutrients']['CHOLE']['quantity'] ?? 'N/A'}",
+              style: GoogleFonts.quicksand(
+                fontSize: 17.5,
+                fontWeight: FontWeight.w500,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+        body: ListView(
+          children: [
+            Container(
+              height: 240.0,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/images/home_bg.jpg"),
+                    fit: BoxFit.fill),
+              ),
+              child: Center(
+                child: Text(
+                  "Welcome to CalorMe!",
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.questrial(
+                    textStyle: const TextStyle(
+                      fontSize: 40.0,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          if (imageFile != null) ...[Image.file(imageFile!)],
-        ],
+            if (imageFile != null) ...[Image.file(imageFile!)],
+          ],
+        ),
       ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: () {
-      //     //TODO: Allow users to choose from gallery too
-      //     _picker.pickImage(source: ImageSource.camera);
-      //   },
-      //   tooltip: "Take or Select an Image",
-      //   backgroundColor: Colors.purple,
-      //   child: const Icon(Icons.add_a_photo, color: Colors.white),
-      // ),
       bottomNavigationBar: BottomAppBar(
         color: Colors.purple[100],
         child: Row(
