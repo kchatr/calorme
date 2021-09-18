97ee6833044d466ea5db81d1c0f673539e038140
kchatr <kchatr1729@gmail.com>
2021-09-18 09:09:05 -0400

    Refactored w/ Constant Constructors

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: "CalorMe",
       home: HomePage(),
     );
